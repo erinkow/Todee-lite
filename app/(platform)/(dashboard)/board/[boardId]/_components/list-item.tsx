f94b5a04190c875d7b0c1ecb1b9edd8b770d48ea
erinkow <eriko034@gmail.com>
2024-07-04 08:50:11 +0900

    dnd function added

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -9,9 +9,11 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { createList } from "@/action/create-list";
 import { toast } from "sonner";
 import { ListForm } from "./list-form";
+import { Draggable } from "@hello-pangea/dnd";
 
 interface ListItemProps {
     data: List;
+    index: number;
     // isEditing: boolean;
     // enableEditing: () => void;
     // disableEditing: () => void;
@@ -19,24 +21,35 @@ interface ListItemProps {
 
 export const ListItem = ({
     data,
+    index
     // isEditing,
     // enableEditing,
     // disableEditing
 }: ListItemProps) => {
-
     return(
-        <li 
-            key={data.id}
-            className="shrink-0 h-auto w-[272px] select-none mb-3 bg-neutral-50 border-none shadow-lg"
+        <Draggable
+            draggableId={data.id}
+            index={index}
         >
-            <div className="">
-                <ListHeader
-                    data={data}
-                    // isEditing={isEditing}
-                    // enableEditing={enableEditing}
-                    // disableEditing={disableEditing}
-                />
-            </div>
-        </li>
+            {(provided) => (
+                    <li 
+                        {...provided.draggableProps}
+                        ref={provided.innerRef}
+                        key={data.id}
+                        className="shrink-0 h-auto w-[272px] select-none mb-3 bg-neutral-50 border-none shadow-lg"
+                    >
+                        <div {...provided.dragHandleProps}>
+                            <ListHeader
+                                data={data}
+                                // isEditing={isEditing}
+                                // enableEditing={enableEditing}
+                                // disableEditing={disableEditing}
+                            />
+                        </div>
+                    </li>
+                )
+            }
+
+        </Draggable>
     )
 }
