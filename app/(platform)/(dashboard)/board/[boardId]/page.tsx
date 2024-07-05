09d51eefd61657a257ddca154e507ccf374b4744
erinkow <eriko034@gmail.com>
2024-07-05 10:25:32 +0900

    adjustment before build -remove React.FC on BoardIdPage

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,31 +1,28 @@
-import { Button } from "@/components/ui/button";
 import { auth } from "@clerk/nextjs/server";
-import { Link } from "lucide-react";
 import { redirect } from "next/navigation";
 import { ListContainer } from "./_components/list-container";
-import { List } from "@prisma/client";
 import { db } from "@/lib/db";
 
-interface BoardIdPageProps {
-    params: {
-        boardId: string;
-    },
-    data: List,
-}
+// interface BoardIdPageProps {
+//     params: {
+//         boardId: string;
+//     }
+// }
 
-const BoardIdPage: React.FC<BoardIdPageProps>= async ({
-    params,
-    data
-}) => {
+const BoardIdPage = async (
+//     {
+//     params,
+// }: BoardIdPageProps
+) => {
     const { userId } = auth();
-    console.log(userId)
-    console.log('params userId', params.boardId);
+    // console.log(userId)
+    // console.log('params userId', params.boardId);
 
     if(!userId) {
         redirect('/sign-in');
     }
 
-    console.log('params.boardId', params.boardId)
+    // console.log('params.boardId', params.boardId)
 
     const boardId = userId
 
