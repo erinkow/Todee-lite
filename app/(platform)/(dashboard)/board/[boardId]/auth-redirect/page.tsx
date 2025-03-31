58e263c6eeb88bf2bf21d37df386be46dffd9ab7
erinkow <eriko034@gmail.com>
2025-03-31 13:35:46 +0900

    error handled on auth-redirect/page.tsx

--- app/(platform)/(dashboard)/board/[boardId]/auth-redirect/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/auth-redirect/page.tsx
@@ -3,13 +3,23 @@ import { useAuth } from "@clerk/nextjs";
 import { useEffect } from "react";
 import { redirect, useRouter } from "next/navigation";  // App Routerでは 'next/navigation' を使用
 
-export default async function AuthRedirect() {
+export default function AuthRedirect() {
   const { userId, isLoaded } = useAuth();
-  
-  if(userId) {
-    const boardId = userId;
-    redirect(`board/${boardId}`)
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoaded) {
+      if (userId) {
+        router.push(`/board/${userId}`);
+      } else {
+        router.push('/signin');
+      }
+    }
+  }, [isLoaded, userId, router]);
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
   }
-  
-  redirect('/signin')
+
+  return null;
 }
